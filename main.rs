9b713836ba981f9289f6f7d344a43c942c095002
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-01-02 01:25:07 -0800

    Implemented connection two points on a board.

--- main.rs
+++ main.rs
@@ -4,31 +4,52 @@
 use std::fmt;
 
 struct Board {
-    point_count: u16,
-    neighbor_lists: Vec<Vec<u16>>,
-    //connectivity_matrix: Vec<Vec<bool>>
+    point_count: usize,
+    neighbor_lists: Vec<Vec<usize>>,
+    connectivity_matrix: Vec<Vec<bool>>
 }
 
 impl Board {
-    fn new(point_count: u16) -> Board {
+    fn new(point_count: usize) -> Board {
         Board {
             point_count: point_count,
-            neighbor_lists: vec![vec![]; point_count as usize],
-            //connectivity_matrix: vec![vec![false; point_count as usize]; point_count as usize]
+            neighbor_lists: vec![vec![]; point_count],
+            connectivity_matrix: vec![vec![false; point_count]; point_count]
+        }
+    }
+}
+
+impl Board {
+    fn connect(&mut self, point_a: usize, point_b: usize) {
+        assert!(point_a < self.point_count);
+        assert!(point_b < self.point_count);
+        assert!(point_a != point_b);
+
+        if !self.connectivity_matrix[point_a][point_b] {
+            self.connectivity_matrix[point_a][point_b] = true;
+            self.connectivity_matrix[point_b][point_a] = true;
+            self.neighbor_lists[point_a].push(point_b);
+            self.neighbor_lists[point_b].push(point_a);
         }
     }
 }
 
 impl fmt::Debug for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{Board; {} points, {} connections}}",
+        let connection_count = 
+            self.neighbor_lists.iter().map(|ls| ls.len()).sum::<usize>() / 2;
+
+        write!(f, "{{Board; {} points, {} connection{}}}",
                self.point_count,
-               self.neighbor_lists.iter().map(|ls| ls.len()).sum::<usize>())
+               connection_count,
+               if connection_count == 1 {""} else {"s"})
     }
 }
 
 fn main() {
-    let board = Board::new(10);
+    let mut board = Board::new(10);
+    board.connect(3, 4);
+    board.connect(4, 9);
     println!("{:?}", board);
 }
 
