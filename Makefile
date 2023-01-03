71d6bbcca9c72695bf8f58d31a157d31f80fe460
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-01-02 23:12:47 -0800

    Moving to cargo.

--- Makefile
+++
@@ -1,6 +0,0 @@
-
-all:
-	@clear
-	@rustc main.rs -o stones
-	@./stones
-
--- main.rs
+++ main.rs
@@ -7,12 +7,14 @@ use engine::Position;
 
 fn main() {
     let board = Board::new(10, (0..9).map(|n| (n, n+1)).collect());
-    let mut p: Position = (*****(&(&(&(&(&board)))))).empty_position();
+    let mut p: Position = board.empty_position();
 
     println!("{:?}", p);
     for point in [1, 3, 2, 4].iter() {
         board.play_black(&mut p, *point);
         println!("{:?}", &p);
     }
+
+    println!("{}", "hello".green());
 }
 
