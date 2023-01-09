e58ad24e44b1e89357a30a199a9c6d9d5f8b8688
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-01-08 23:16:15 -0800

    Cleaned up main.rs.

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,11 @@ mod engine;
 mod boards;
 mod interactive;
 
-use engine::Color::*;
-use engine::Position;
 use boards::*;
 use interactive::*;
 
 fn main() {
-    let (board, layout) = make_rectangular_board(6, 2);
-    let mut p: Position = board.empty_position();
-
-    println!("Original board state");
-    print!("{:?}", p);
-
-    let program = [
-        (Black, 3),
-        (White, 2),
-        (White, 4),
-        (White, 9),
-    ];
-
-    for (color, point) in program {
-        println!("=================");
-        println!("Playing move at {} (color: {})", point, color as usize);
-        p.play(point, color);
-        print!("{:?}", &p);
-    }
+    let (board, layout) = make_rectangular_board(9, 9);
 
     interactive_app(board, layout);
 }
