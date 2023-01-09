6c9008324e8cf27801d9343cd83304b421b7437b
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-01-09 00:44:39 -0800

    Added loop boards.

--- src/boards.rs
+++ src/boards.rs
@@ -1,23 +1,40 @@
 
 use crate::engine::Board;
 
-pub fn make_rectangular_board(width: usize, height: usize) -> (Board, Vec<(f32, f32)>) {
+type Layout = Vec::<(f32, f32)>;
+type Edges = Vec::<(usize, usize)>;
+type Bal = (Board, Vec<(f32, f32)>); // "Board and Layout"
+
+pub fn make_rectangular_board(width: usize, height: usize) -> Bal {
     let size = width*height;
-    let mut connections = Vec::<(usize, usize)>::new();
-    let mut locations = Vec::<(f32, f32)>::new();
+    let mut edges = Edges::new();
+    let mut layout = Layout::new();
 
     for point in 0..size {
         let x = point % width;
         let y = point / width;
 
-        if x > 0 {connections.push((point, point - 1));}
-        if y > 0 {connections.push((point, point - width));}
+        if x > 0 {edges.push((point, point - 1));}
+        if y > 0 {edges.push((point, point - width));}
 
-        locations.push((x as f32, y as f32));
+        layout.push((x as f32, y as f32));
     }
 
-    let mut board = Board::new(size, connections);
+    let mut board = Board::new(size, edges);
     board.set_tui_layout((0..size).map(|n| (n % width, n / width)).collect());
-    (board, locations)
+    (board, layout)
+}
+
+pub fn make_loop_board(n: usize) -> Bal {
+    let mut edges = Edges::new();
+    let mut layout = Layout::new();
+
+    for point in 0 .. n {
+        let theta = ((point as f32) / (n as f32) - 0.25) * std::f32::consts::PI * 2.0;
+        layout.push((theta.cos(), theta.sin()));
+        edges.push((point, (point + 1) % n));
+    }
+
+    (Board::new(n, edges), layout)
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ use boards::*;
 use interactive::*;
 
 fn main() {
-    let (board, layout) = make_rectangular_board(9, 9);
+    //let (board, layout) = make_rectangular_board(9, 9);
+    let (board, layout) = make_loop_board(3);
 
     interactive_app(board, layout);
 }
