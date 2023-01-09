038e5ef3a345b3696b1ffb955d35964dbca5d562
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-01-08 17:17:49 -0800

    Got an SFML window to open.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use engine::Color::*;
 use engine::Board;
 use engine::Position;
 use boards::*;
+use sfml::graphics::*;
+use sfml::window::*;
 
 fn main() {
     let board = make_rectangular_board(6, 2);
@@ -28,6 +30,20 @@ fn main() {
         println!("Playing move at {} (color: {})", point, color as usize);
         p.play(point, color);
         print!("{:?}", &p);
+    }
+
+    let mut window = Window::new((800, 600), "Window", Style::NONE, &Default::default());
+    window.set_framerate_limit(60);
+
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            if event == Event::Closed {
+                window.close();
+            }
+        }
+
+        window.set_active(true);
+        window.display();
     }
 }
 
