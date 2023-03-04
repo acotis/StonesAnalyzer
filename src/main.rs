563bd5bbf4b5de34f9d632cb182989f7b6dcc546
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-03-03 20:46:30 -0800

    Tweaks and studying.

--- src/main.rs
+++ src/main.rs
@@ -22,17 +22,41 @@ use boards::*;
 fn main() -> io::Result<()> {
     env::set_var("RUST_BACKTRACE", "1");
 
-    let filename = "analyses/loop-5.san";
-    //let filename = "analyses/stupid_8.san";
-    //let filename = "analyses/simple.san";
+    //let layout = vec![
+        //(0.0,0.0),(1.0,0.0),(2.0,0.0),(3.0,0.0),(4.0,0.0),
+        //(0.0,1.0),(1.0,1.0),(2.0,1.0),(3.0,1.0),(4.0,1.0),
+        //(0.0,2.0),(1.0,2.0),(2.0,2.0),(3.0,2.0),(4.0,2.0),
+        //(0.0,3.0),(1.0,3.0),(2.0,3.0),
+    //];
+    //let board = Board::new(vec![
+        //(0,1),(1,2),(2,3),(3,4),
+        //(5,6),(6,7),(7,8),(8,9),
+        //(10,11),(11,12),(12,13),(13,14),
+        //(15,16),(16,17),
+        //(0,5),(5,10),(10,15),
+        //(1,6),(6,11),(11,16),
+        //(2,7),(7,12),(12,17),
+        //(3,8),(8,13),
+        //(4,9),(9,14),
+    //]);
+    //analyze_and_create_san_file("no-name", (board, layout));
 
-    let (mut gametree, layout) = read_san_file(filename)?;
-    //let (board, layout) = make_loop_board(5);
-    //let mut gametree = GameTree::new(board);
+    analyze_existing_san_file("analyses/2023-03-03.san")?;
 
+    Ok(())
+}
+
+fn analyze_and_create_san_file(filename: &str, (board, layout): (Board, Layout)) -> io::Result<()> {
+    let mut gametree = GameTree::new(board);
     interactive_app(&mut gametree, &layout);
     write_san_file(filename, gametree, layout)?;
+    Ok(())
+}
 
+fn analyze_existing_san_file(filename: &str) -> io::Result<()> {
+    let (mut gametree, layout) = read_san_file(filename)?;
+    interactive_app(&mut gametree, &layout);
+    write_san_file(filename, gametree, layout)?;
     Ok(())
 }
 
