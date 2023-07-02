16306b9b9988e2883218f98810c9c221328f525f
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-07-01 22:03:53 -0700

    Studying; mostly implemented SVG generation program.

--- src/svg.rs
+++ src/svg.rs
@@ -1,39 +1,52 @@
 
-fn hex_svg() {
-    let layout = layout_trihex(5);
-    let edges = edges_trihex(5);
+use stones::boards::Lae;
+use stones::boards::lae_from_spec;
 
-    let scale = 100.0; // "pixels" per inch
-    let len = 0.875 * scale;
-    //let margin = 0.5 * scale;
-    let window = 5.5 * scale;
-    let width = 0.03125 * scale;
+fn svg_from_lae(lae: Lae) -> String {
+    let scale = 300.0;      // "pixels" per inch
+    let diam = 0.875;       // stone diameter in inches
+    let width = 0.03125;    // width of each edge line in inches
+    let margin = 0.5;       // shortest distance between a stone's edge and the board's edge
+    let window = 0.5;       // shortest distance between the board's edge and the image's edge
 
-    let x = layout[45].0;
-    let y = layout[45].1;
+    // Private computations.
 
-    println!("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-    println!("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"960\" height=\"960\" viewBox=\"-{} -{} {} {}\">",
-             window, window, window * 2.0, window * 2.0);
-    print!("<polygon fill=\"#DCB35C\" points=\"");
+    let (layout, edges) = lae;
+    let distance = diam / 2.0 + margin + window;
 
-    for i in 0..6 {
-        //let radius = len * 5.0 + margin;
-        let radius = 5.0 * scale;
-        let x = ((i as f32) * TAU / 6.0).cos() * radius;
-        let y = ((i as f32) * TAU / 6.0).sin() * radius;
-        print!("{},{} ", x, y)
-    }
+    let left   = scale * (layout.iter().map(|&n| n.0 * diam).reduce(f32::min).unwrap() - distance);
+    let right  = scale * (layout.iter().map(|&n| n.0 * diam).reduce(f32::max).unwrap() + distance);
+    let top    = scale * (layout.iter().map(|&n| n.1 * diam).reduce(f32::min).unwrap() - distance);
+    let bottom = scale * (layout.iter().map(|&n| n.1 * diam).reduce(f32::max).unwrap() + distance);
 
-    println!("\"/>");
-    print!("<path stroke=\"#000\" stroke-width=\"{}\" fill=\"none\" d=\"", width);
+    // Stroke pattern.
+    
+    let mut strokes = "".to_string();
 
     for edge in edges {
-        print!("M {} {} ", (layout[edge.0].0 - x) * len, (layout[edge.0].1 - y) * len);
-        print!("L {} {} ", (layout[edge.1].0 - x) * len, (layout[edge.1].1 - y) * len);
+        strokes.push_str(&format!("M {} {} ", layout[edge.0].0 * diam * scale, layout[edge.0].1 * diam * scale));
+        strokes.push_str(&format!("L {} {} ", layout[edge.1].0 * diam * scale, layout[edge.1].1 * diam * scale));
     }
 
-    println!("\"/>");
-    println!("</svg>");
+    // SVG data.
+
+    let mut ret = "".to_string();
+
+    ret.push_str(&format!("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"));
+    ret.push_str(&format!("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"{} {} {} {}\">\n",
+             left, top, right - left, bottom - top));
+    ret.push_str(&format!("<path stroke=\"#DCB35C\" stroke-linecap=\"round\" stroke-width=\"{}\" fill=\"none\" d=\"", scale * (diam / 2.0 + margin)));
+    ret.push_str(&strokes);
+    ret.push_str("\"/>\n");
+    ret.push_str(&format!("<path stroke=\"#000\" stroke-linecap=\"round\" stroke-width=\"{}\" fill=\"none\" d=\"", scale * width));
+    ret.push_str(&strokes);
+    ret.push_str("\"/>\n");
+    ret.push_str("</svg>\n");
+
+    ret
+}
+
+fn main() {
+    println!("{}", svg_from_lae(lae_from_spec("trihex:5").unwrap()));
 }
 
