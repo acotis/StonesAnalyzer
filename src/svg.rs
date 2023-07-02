a0a90cfc77dcc8ed159f0a88a2a0d7894c8d7390
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-07-01 23:14:37 -0700

    Added stone-rendering ability to SVG binary.

--- src/svg.rs
+++ src/svg.rs
@@ -1,31 +1,34 @@
 
 use stones::boards::Lae;
+use stones::boards::Layout;
 use stones::boards::lae_from_spec;
+use stones::engine::{Board, Position, Color::*};
 
-fn svg_from_lae(lae: Lae) -> String {
+fn svg_from_lae(lae: Lae, position: Position) -> String {
     let scale = 300.0;      // "pixels" per inch
     let diam = 0.875;       // stone diameter in inches
     let width = 0.03125;    // width of each edge line in inches
-    let margin = 0.5;       // shortest distance between a stone's edge and the board's edge
-    let window = 0.5;       // shortest distance between the board's edge and the image's edge
+    let margin = 0.15;       // shortest distance between a stone's edge and the board's edge
+    let window = 0.0;       // shortest distance between the board's edge and the image's edge
 
     // Private computations.
 
-    let (layout, edges) = lae;
+    let (layout_stone, edges) = lae;
+    let layout: Layout = layout_stone.into_iter().map(|p| (p.0 * diam, p.1 * diam)).collect();
     let distance = diam / 2.0 + margin + window;
 
-    let left   = scale * (layout.iter().map(|&n| n.0 * diam).reduce(f32::min).unwrap() - distance);
-    let right  = scale * (layout.iter().map(|&n| n.0 * diam).reduce(f32::max).unwrap() + distance);
-    let top    = scale * (layout.iter().map(|&n| n.1 * diam).reduce(f32::min).unwrap() - distance);
-    let bottom = scale * (layout.iter().map(|&n| n.1 * diam).reduce(f32::max).unwrap() + distance);
+    let left   = scale * (layout.iter().map(|&n| n.0).reduce(f32::min).unwrap() - distance);
+    let right  = scale * (layout.iter().map(|&n| n.0).reduce(f32::max).unwrap() + distance);
+    let top    = scale * (layout.iter().map(|&n| n.1).reduce(f32::min).unwrap() - distance);
+    let bottom = scale * (layout.iter().map(|&n| n.1).reduce(f32::max).unwrap() + distance);
 
     // Stroke pattern.
     
     let mut strokes = "".to_string();
 
     for edge in edges {
-        strokes.push_str(&format!("M {} {} ", layout[edge.0].0 * diam * scale, layout[edge.0].1 * diam * scale));
-        strokes.push_str(&format!("L {} {} ", layout[edge.1].0 * diam * scale, layout[edge.1].1 * diam * scale));
+        strokes.push_str(&format!("M {} {} ", layout[edge.0].0 * scale, layout[edge.0].1 * scale));
+        strokes.push_str(&format!("L {} {} ", layout[edge.1].0 * scale, layout[edge.1].1 * scale));
     }
 
     // SVG data.
@@ -35,18 +38,39 @@ fn svg_from_lae(lae: Lae) -> String {
     ret.push_str(&format!("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"));
     ret.push_str(&format!("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"{} {} {} {}\">\n",
              left, top, right - left, bottom - top));
-    ret.push_str(&format!("<path stroke=\"#DCB35C\" stroke-linecap=\"round\" stroke-width=\"{}\" fill=\"none\" d=\"", scale * (diam / 2.0 + margin)));
+    ret.push_str(&format!("<path stroke=\"#DCB35C\" stroke-linecap=\"round\" stroke-width=\"{}\" fill=\"none\" d=\"", scale * (diam + margin * 2.0)));
     ret.push_str(&strokes);
     ret.push_str("\"/>\n");
     ret.push_str(&format!("<path stroke=\"#000\" stroke-linecap=\"round\" stroke-width=\"{}\" fill=\"none\" d=\"", scale * width));
     ret.push_str(&strokes);
     ret.push_str("\"/>\n");
+
+    for i in 0..layout.len() {
+        if position[i] != Empty {
+            ret.push_str(&format!("<circle fill=\"{}\" cx=\"{}\" cy=\"{}\" r=\"{}\"/>\n",
+                                  if position[i] == Black {"#000"} else {"#FFF"},
+                                  layout[i].0 * scale, layout[i].1 * scale, scale * diam / 2.0));
+        }
+    }
+
     ret.push_str("</svg>\n");
 
     ret
 }
 
+fn svg_from_lae_blank(lae: Lae) -> String {
+    let board = Board::new(lae.1.clone());
+    svg_from_lae(lae, board.empty_position())
+}
+
 fn main() {
-    println!("{}", svg_from_lae(lae_from_spec("trihex:5").unwrap()));
+    let lae = lae_from_spec("turtle:4:4").unwrap();
+    let board = Board::new(lae.1.clone());
+    let mut position = board.empty_position();
+
+    board.play(&mut position, Black, 1);
+    board.play(&mut position, White, 5);
+
+    println!("{}", svg_from_lae(lae, position));
 }
 
