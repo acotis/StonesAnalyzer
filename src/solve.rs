21d8c8a9af78b01fa70cc48276cf4d928b14dd80
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-10-08 09:58:15 -0700

    Added command line argument to solver.

--- src/solve.rs
+++ src/solve.rs
@@ -1,5 +1,29 @@
 
+#![deny(warnings)]
+
+use std::env;
+use clap::Parser;
+
+use stones::boards::lae_from_spec;
+
+// Command-line arguments.
+
+#[derive(Parser)]
+struct CLI {
+    #[arg()] board_spec: String,
+}
+
 fn main() {
-    println!("Hello world, this is the solver.");
+    env::set_var("RUST_BACKTRACE", "1");
+    let args = CLI::parse();
+
+    let lae = lae_from_spec(&args.board_spec);
+    
+    if let Err(err_string) = lae {
+        eprintln!("{}", err_string);
+        return;
+    }
+
+    let (_layout, _edges) = lae.unwrap();
 }
 
