f332490e30eae47c44ff3cd6655b3dead88fa3c4
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-10-08 10:07:50 -0700

    Fixed typos.

--- src/solve.rs
+++ src/solve.rs
@@ -5,6 +5,8 @@ use std::env;
 use clap::Parser;
 
 use stones::boards::lae_from_spec;
+use stones::gametree::GameTree;
+use stones::engine::Board;
 
 // Command-line arguments.
 
@@ -17,14 +19,18 @@ fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     let args = CLI::parse();
 
+    // Create the game tree from the board spec passed on the command line.
+
     let lae = lae_from_spec(&args.board_spec);
-    
-    let (_layout, _edges) = match lae {
+    let (layout, edges) = match lae {
         Err(err_string) => {
             eprintln!("{}", err_string);
             return;
         },
         Ok(result) => result
     };
+
+    let _point_count = layout.len();
+    let _game_tree = GameTree::new(Board::new(edges));
 }
 
