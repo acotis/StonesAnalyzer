2297a6983eceb9a8ece96c87cb2886ecfb0addb5
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2023-10-09 18:16:28 -0700

    Moved some stuff from Position to Board that was already there in spirit.

--- src/lib/engine.rs
+++ src/lib/engine.rs
@@ -139,32 +139,27 @@ impl Board {
         }
     }
 
-    pub fn play(&self, pos: &mut Position, color: Color, point: usize) {
-        pos.play(self, color, point);
-    }
 
-    pub fn keep_only_immortal(&self, pos: &mut Position) {
-        pos.keep_only_immortal(self);
-    }
-}
 
-impl Position {
+
+    // Post-refactor material below.
+
 
     // Play a stone of a given color at a given point.
 
-    fn play(&mut self, board: &Board, color: Color, point: usize) {
+    pub fn play(&self, pos: &mut Position, color: Color, point: usize) {
         assert!(color != Empty);
-        assert!(self.board_state[point] == Empty);
+        assert!(pos[point] == Empty);
         
         // For later, note the ID of the bubble at this point.
 
-        let bubble_id = self.chain_id_backref[point];
+        let bubble_id = pos.chain_id_backref[point];
 
         // Place the stone and seed a new chain from it. This will merge any
         // existing chains that are adjacent to the point it was played at.
         
-        self.board_state[point] = color;
-        self.seed_chain(board, point);
+        pos.board_state[point] = color;
+        self.seed_chain(pos, point);
 
         // This move may be splitting the bubble it was played in into multiple
         // parts. For each empty point adjacent to the move, we will seed a new
@@ -172,31 +167,179 @@ impl Position {
         // seeding process initiated by a previous adjacent point, we do not
         // need to seed a new chain there.
 
-        for &neighbor in board.neighbor_lists[point].iter() {
-            if self.chain_id_backref[neighbor] == bubble_id {
-                self.seed_chain(board, neighbor);
+        for &neighbor in self.neighbor_lists[point].iter() {
+            if pos.chain_id_backref[neighbor] == bubble_id {
+                self.seed_chain(pos, neighbor);
             }
         }
 
         // Perform captures.
         
-        self.capture(board, color.reverse());
-        self.capture(board, color);
+        self.capture(pos, color.reverse());
+        self.capture(pos, color);
     }
+
+
+
 
     // Keep only immortal stones.
 
-    fn keep_only_immortal(&mut self, board: &Board) {
-        let mut immortal_white = self.clone();
-        immortal_white.keep_only_immortal_one_color(board, White);
-        self.keep_only_immortal_one_color(board, Black);
+    pub fn keep_only_immortal(&self, pos: &mut Position) {
+        let mut immortal_white = pos.clone();
+        self.keep_only_immortal_one_color(&mut immortal_white, White);
+        self.keep_only_immortal_one_color(pos, Black);
 
-        for i in 0..board.point_count {
+        for i in 0..self.point_count {
             if immortal_white[i] == White {
-                self.play(board, White, i);
+                self.play(pos, White, i);
             }
         }
     }
+
+    // Use the bucketfill algorithm to create a chain from a given seed point.
+    // Each time you add a point to the new chain, remove it from the chain it
+    // started in and update the backref. The ID of the new chain is guaranteed
+    // to be unequal to that of any chain that existed when the method was called.
+
+    fn seed_chain(&self, pos: &mut Position, point: usize) -> usize {
+        let id = pos.fresh_chain_id();
+        let color = pos[point];
+
+        pos.remove_from_chain(pos.chain_id_backref[point], point);
+        pos.add_to_chain(id, point);
+
+        let mut next = 0;
+
+        while next < pos.chains[id].len() {
+            let point = pos.chains[id][next];
+
+            for &neighbor in self.neighbor_lists[point].iter() {
+                if pos[neighbor] == color {
+                    let current_chain = pos.chain_id_backref[neighbor];
+                    if current_chain != id {
+                        pos.remove_from_chain(current_chain, neighbor);
+                        pos.add_to_chain(id, neighbor);
+                    }
+                }
+            }
+
+            next += 1;
+        }
+
+        id
+    }
+
+    // Remove a given chain (i.e. set all its points to empty and update the
+    // chain list).
+
+    fn remove_chain(&self, pos: &mut Position, id: usize) {
+        assert!(!pos.chains[id].is_empty());
+
+        for &point in pos.chains[id].iter() {
+            pos.board_state[point] = Empty;
+        }
+
+        self.seed_chain(pos, pos.chains[id][0]);
+    }
+
+    // Capture all surrounded chains of a given color.
+
+    fn capture(&self, pos: &mut Position, color: Color) {
+        for id in 0..pos.chains.len() {
+            if pos.chains[id].is_empty() {continue;}
+            if pos[pos.chains[id][0]] != color {continue;}
+
+            if pos.chains[id].iter()
+                   .any(|&n| self.neighbor_lists[n].iter()
+                                 .any(|&n| pos[n] == Empty)) {continue;}
+
+            self.remove_chain(pos, id);
+        }
+    }
+
+    // Check whether a given chain has another chain as a foot (a bubble whose
+    // every point is a liberty of the chain). This only means anything after
+    // clearing all chains of the *opposite* color off the board.
+
+    fn check_if_foot(&self, pos: &Position, chain_id: usize, bubble_id: usize) -> bool {
+        if pos.chains[bubble_id].is_empty() {return false;}
+        if pos[pos.chains[bubble_id][0]] != Empty {return false;}
+
+        pos.chains[bubble_id].iter()
+            .all(|&point| self.neighbor_lists[point].iter()
+                          .any(|&neighbor| pos.chain_id_backref[neighbor] == chain_id))
+    }
+
+    // Check whether a given chain has two feet. This only means anything after
+    // clearing all chains of the *opposite* color off the board.
+
+    fn check_if_standing(&self, pos: &Position, chain_id: usize) -> bool {
+        let mut adjacent_bubbles = Vec::<usize>::new();
+
+        for &point in pos.chains[chain_id].iter() {
+            for &neighbor in self.neighbor_lists[point].iter() {
+                if pos[neighbor] == Empty {
+                    adjacent_bubbles.push(pos.chain_id_backref[neighbor]);
+                }
+            }
+        }
+
+        adjacent_bubbles.sort();
+        adjacent_bubbles.dedup();
+
+        let mut foot_count = 0;
+
+        for ab in adjacent_bubbles {
+            if self.check_if_foot(pos, chain_id, ab) {
+                foot_count += 1;
+                if foot_count == 2 {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // Clear all chains of a given color off the board.
+
+    fn clear_color(&self, pos: &mut Position, color: Color) {
+        let chains_to_clear: Vec<usize> = 
+            (0..pos.chains.len())
+                .filter(|&n| !pos.chains[n].is_empty())
+                .filter(|&n| pos[pos.chains[n][0]] == color)
+                .collect();
+
+        for chain in chains_to_clear {
+            self.remove_chain(pos, chain);
+        }
+    }
+
+    // Keep only immortal chains of a given color.
+
+    fn keep_only_immortal_one_color(&self, pos: &mut Position, color: Color) {
+        self.clear_color(pos, color.reverse());
+        
+        loop {
+            let to_clear: Vec<usize> =
+                (0..pos.chains.len())
+                .filter(|&n| !pos.chains[n].is_empty())
+                .filter(|&n| pos[pos.chains[n][0]] == color)
+                .filter(|&n| !self.check_if_standing(pos, n))
+                .collect();
+
+            if to_clear.is_empty() {
+                break;
+            }
+
+            for chain in to_clear {
+                self.remove_chain(pos, chain);
+            }
+        }
+    }
+}
+
+impl Position {
 
     // Return the ID of a currently unused chain vector.
 
@@ -220,148 +363,11 @@ impl Position {
         self.chains[id].swap_remove(index);
     }
 
-    // Use the bucketfill algorithm to create a chain from a given seed point.
-    // Each time you add a point to the new chain, remove it from the chain it
-    // started in and update the backref. The ID of the new chain is guaranteed
-    // to be unequal to that of any chain that existed when the method was called.
+    // Add a point to a given chain.
 
-    fn seed_chain(&mut self, board: &Board, point: usize) -> usize {
-        let id = self.fresh_chain_id();
-        let color = self.board_state[point];
-
-        self.remove_from_chain(self.chain_id_backref[point], point);
+    fn add_to_chain(&mut self, id: usize, point: usize) {
         self.chains[id].push(point);
         self.chain_id_backref[point] = id;
-
-        let mut next = 0;
-
-        while next < self.chains[id].len() {
-            let point = self.chains[id][next];
-
-            for &neighbor in board.neighbor_lists[point].iter() {
-                if self.board_state[neighbor] == color {
-                    let current_chain = self.chain_id_backref[neighbor];
-                    if current_chain != id {
-                        self.remove_from_chain(current_chain, neighbor);
-                        self.chains[id].push(neighbor);
-                        self.chain_id_backref[neighbor] = id;
-                    }
-                }
-            }
-
-            next += 1;
-        }
-
-        id
-    }
-
-    // Remove a given chain (i.e. set all its points to empty and update the
-    // chain list).
-
-    fn remove_chain(&mut self, board: &Board, id: usize) {
-        assert!(!self.chains[id].is_empty());
-
-        for &point in self.chains[id].iter() {
-            self.board_state[point] = Empty;
-        }
-
-        self.seed_chain(board, self.chains[id][0]);
-    }
-
-    // Capture all surrounded chains of a given color.
-
-    fn capture(&mut self, board: &Board, color: Color) {
-        for id in 0..self.chains.len() {
-            if self.chains[id].is_empty() {continue;}
-            if self.board_state[self.chains[id][0]] != color {continue;}
-
-            if self.chains[id].iter()
-                   .any(|&n| board.neighbor_lists[n].iter()
-                                 .any(|&n| self.board_state[n] == Empty)) {continue;}
-
-            self.remove_chain(board, id);
-        }
-    }
-
-    // Check whether a given chain has another chain as a foot (a bubble whose
-    // every point is a liberty of the chain). This only means anything after
-    // clearing all chains of the *opposite* color off the board.
-
-    fn check_if_foot(&self, board: &Board, chain_id: usize, bubble_id: usize) -> bool {
-        if self.chains[bubble_id].is_empty() {return false;}
-        if self[self.chains[bubble_id][0]] != Empty {return false;}
-
-        self.chains[bubble_id].iter()
-            .all(|&point| board.neighbor_lists[point].iter()
-                          .any(|&neighbor| self.chain_id_backref[neighbor] == chain_id))
-    }
-
-    // Check whether a given chain has two feet. This only means anything after
-    // clearing all chains of the *opposite* color off the board.
-
-    fn check_if_protected(&self, board: &Board, chain_id: usize) -> bool {
-        let mut adjacent_bubbles = Vec::<usize>::new();
-
-        for &point in self.chains[chain_id].iter() {
-            for &neighbor in board.neighbor_lists[point].iter() {
-                if self.board_state[neighbor] == Empty {
-                    adjacent_bubbles.push(self.chain_id_backref[neighbor]);
-                }
-            }
-        }
-
-        adjacent_bubbles.sort();
-        adjacent_bubbles.dedup();
-
-        let mut foot_count = 0;
-
-        for ab in adjacent_bubbles {
-            if self.check_if_foot(board, chain_id, ab) {
-                foot_count += 1;
-                if foot_count == 2 {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    // Clear all chains of a given color off the board.
-
-    fn clear_color(&mut self, board: &Board, color: Color) {
-        let chains_to_clear: Vec<usize> = 
-            (0..self.chains.len())
-                .filter(|&n| !self.chains[n].is_empty() && 
-                        self.board_state[self.chains[n][0]] == color)
-                .collect();
-
-        for chain in chains_to_clear {
-            self.remove_chain(board, chain);
-        }
-    }
-
-    // Keep only immortal chains of a given color.
-
-    fn keep_only_immortal_one_color(&mut self, board: &Board, color: Color) {
-        self.clear_color(board, color.reverse());
-        
-        loop {
-            let to_clear: Vec<usize> =
-                (0..self.chains.len())
-                .filter(|&n| !self.chains[n].is_empty() &&
-                        self.board_state[self.chains[n][0]] == color &&
-                        !self.check_if_protected(board, n))
-                .collect();
-
-            if to_clear.is_empty() {
-                break;
-            }
-
-            for chain in to_clear {
-                self.remove_chain(board, chain);
-            }
-        }
     }
 }
 
