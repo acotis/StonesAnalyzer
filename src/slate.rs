cc65e87744ca612a7a5340c7471a6a310b03f157
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-03-21 00:04:13 -0400

    Working on setting up web stuff.

--- src/slate.rs
+++ src/slate.rs
@@ -1,5 +1,22 @@
 
+use reqwest::blocking::Client;
+
 fn main() {
     println!("Hello world");
+
+    let mut client = Client::default();
+
+    let response = 
+        client.get("http://google.com")
+              .send();
+
+    match response {
+        Ok(val) => {
+            println!("Web request successful. Response: {val:?}");
+        },
+        Err(val) => {
+            println!("Error: {val}");
+        },
+    }
 }
 
